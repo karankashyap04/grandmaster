31f2c637ad4c71a69275c4e018a4229ae3674276
karankashyap04 <karankashyap@gmail.com>
2022-12-27 17:02:22 +0530

    chore: Minor edits to inline comments

--- client/src/components/piece/Piece.tsx
+++ client/src/components/piece/Piece.tsx
@@ -1,36 +1,36 @@
 import React from "react";
 
 export default function Piece() {
-  // this piece should take its color and type
+  // this function should take the color and type
   return <div>Piece!</div>;
 }
 
 export function Pawn() {
-  // this piece should take its color
+  // this function should take the color
   return <div>Pawn</div>;
 }
 
 export function Rook() {
-  // this piece should take its color
+  // this function should take the color
   return <div>Rook</div>;
 }
 
 export function Knight() {
-  // this piece should take its color
+  // this function should take the color
   return <div>Knight</div>;
 }
 
 export function Bishop() {
-  // this piece should take its color
+  // this function should take the color
   return <div>Bishop</div>;
 }
 
 export function King() {
-  // this piece should take its color
+  // this function should take the color
   return <div>King</div>;
 }
 
 export function Queen() {
-  // this piece should take its color
+  // this function should take the color
   return <div>Queen</div>;
 }
