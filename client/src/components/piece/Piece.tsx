075ae09575787c5d7f6ba13104d0ff463abe92f5
karankashyap04 <karankashyap@gmail.com>
2022-12-27 17:00:56 +0530

    feat: Create boilerplate structure for Piece component

---
+++ client/src/components/piece/Piece.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Piece() {
+  // this piece should take its color and type
+  return <div>Piece!</div>;
+}
+
+export function Pawn() {
+  // this piece should take its color
+  return <div>Pawn</div>;
+}
+
+export function Rook() {
+  // this piece should take its color
+  return <div>Rook</div>;
+}
+
+export function Knight() {
+  // this piece should take its color
+  return <div>Knight</div>;
+}
+
+export function Bishop() {
+  // this piece should take its color
+  return <div>Bishop</div>;
+}
+
+export function King() {
+  // this piece should take its color
+  return <div>King</div>;
+}
+
+export function Queen() {
+  // this piece should take its color
+  return <div>Queen</div>;
+}
